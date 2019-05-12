3b5ad0bcedb963759bb4ec9428f7a0fe28566c0c
Sergey Sokurenko <s.sokurenko@gmail.com>
2019-05-12 13:11:29 +0200

    Added a Stateful widget and correspondent State

--- lib/main.dart
+++ lib/main.dart
@@ -3,19 +3,39 @@ import 'package:flutter/material.dart';
 // Application entry point
 main() => runApp(App());
 
-class App extends StatelessWidget {
+class App extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _AppState();
+  }
+}
+
+class _AppState extends State<App> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('Cats rock'),
-        ),
-        body: Card(
-            child: Column(
-          children: <Widget>[Image.asset('assets/images/cat.jpg'), Text('Cat')],
-        )),
-      ),
+          appBar: AppBar(
+            title: Text('Cats rock'),
+          ),
+          body: Column(
+            children: [
+              Container(
+                margin: EdgeInsets.all(20.0),
+                child: RaisedButton(
+                  child: Text('Add'),
+                  onPressed: () {},
+                ),
+              ),
+              Card(
+                  child: Column(
+                children: <Widget>[
+                  Image.asset('assets/images/cat.jpg'),
+                  Text('Cat')
+                ],
+              )),
+            ],
+          )),
       debugShowCheckedModeBanner: false,
     );
   }
