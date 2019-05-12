c98353ec016d076c80ebdd47f054e1070163d8de
Sergey Sokurenko <s.sokurenko@gmail.com>
2019-05-12 12:36:58 +0200

    Added initial app scaffolding

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,18 @@
 import 'package:flutter/material.dart';
 
-main() {
+// Application entry point
+main() => runApp(App());
 
-}
-
-class MyApp extends StatelessWidget {
-  build(BuildContext context) {
-    return MaterialApp();
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Flutter Rocks'),
+        ),
+      ),
+      debugShowCheckedModeBanner: false,
+    );
   }
 }
