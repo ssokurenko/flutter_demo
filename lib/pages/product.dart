ccd19a1aba1f308415f50ef636645093ce0f39aa
Sergey Sokurenko <s.sokurenko@gmail.com>
2019-05-18 22:11:50 +0200

    Handling the hardware back button tap'

--- lib/pages/product.dart
+++ lib/pages/product.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
 
 class ProductPage extends StatelessWidget {
-
   final String title;
   final String imageUrl;
 
@@ -9,29 +9,34 @@ class ProductPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-          title: Center(
-        child: Text(title),
-      )),
-      body: Center(
-          child: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: <Widget>[
-          Image.asset(imageUrl),
-          Container(
-            padding: EdgeInsets.all(10.0),
-            child: Text('Details'),
-          ),
-          Container(
-              padding: EdgeInsets.all(10.0),
-              child: RaisedButton(
-                child: Text('Delete'),
-                color: Theme.of(context).primaryColor,
-                onPressed: () => Navigator.pop(context, true),
-              ))
-        ],
-      )),
-    );
+    return WillPopScope(
+        onWillPop: () {
+          Navigator.pop(context, false);
+          return Future.value(false);
+        },
+        child: Scaffold(
+          appBar: AppBar(
+              title: Center(
+            child: Text(title),
+          )),
+          body: Center(
+              child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Image.asset(imageUrl),
+              Container(
+                padding: EdgeInsets.all(10.0),
+                child: Text('Details'),
+              ),
+              Container(
+                  padding: EdgeInsets.all(10.0),
+                  child: RaisedButton(
+                    child: Text('Delete'),
+                    color: Theme.of(context).primaryColor,
+                    onPressed: () => Navigator.pop(context, true),
+                  ))
+            ],
+          )),
+        ));
   }
 }
